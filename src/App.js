4db5cc1a07b8c9bef4d485ecf9b288f6ec5592de
Debbie Fan <debbiehmfan@outlook.com>
2022-11-25 21:25:04 -0500

    Add travel data to the journal entry cards

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
-import React from "react";
+import React from 'react';
 import './index.css'
-import Navbar from "./components/Navbar";
-import Card from "./components/Card";
+import Navbar from './components/Navbar';
+import Card from './components/Card';
+import data from './store/data';
 
 function App() {
+    const cards = data.map((item, i, data) => {
+        return (
+            <div>
+                <Card
+                    key = {item.id}
+                    {...item}
+                />
+                {i + 1 === data.length ? null : <hr/>}
+            </div>
+        )
+    })
+    
     return (
         <div>
             <Navbar/>
-            <Card/>
-            <hr/>
+            <div>
+                {cards}
+            </div>
         </div>
     )
 }
---
+++ src/store/data.js
@@ -0,0 +1,39 @@
+export default [
+    {
+        id: 1,
+        img: 'Buckingham Palace.png',
+        country: 'LONDON, UK',
+        link: 'https://www.google.com/maps/place/Buckingham+Palace/@51.5013673,-0.1440787,17z/data=!3m2!4b1!5s0x487605276d38fb6b:0xe1c60228d7946675!4m5!3m4!1s0x48760520cd5b5eb5:0xa26abf514d902a7!8m2!3d51.501364!4d-0.14189',
+        location: 'Buckingham Palace',
+        startDate: '09 Oct. 2018',
+        description: "Visited on a nice and sunny day. The Queen wasn't at the palace, so we didn't get to see her but I really enjoyed walking around the parks near the palace and seeing the soldiers! The parks in England are very nice with lots of big trees and a lot of people enjoy bringing blankets and hanging out on the open green spaces. Fun fact: this photo I took and edited has been viewed over 2 million times on Unsplash and downloaded over 1000 times! It's been used in articles about the Buckingham palace and a few hotel and trip websites."     
+    },
+    {
+        id: 4,
+        img: 'University of Cambridge.png',
+        country: 'CAMBRIDGE, UK',
+        link: 'https://www.google.com/maps/place/University+of+Cambridge/@52.205356,0.113168,17z/data=!3m1!4b1!4m5!3m4!1s0x47d8774ed42712a9:0x21ca80abf36db5bb!8m2!3d52.205356!4d0.113168',
+        location: 'University of Cambridge',
+        startDate: '02 Nov. 2018',
+        description: "The University of Cambridge is so beautiful! We visited during the afternoon and walked around. In the evening, we took a boat ride around the campus. Fun fact: It was founded in 1209, making it the world's third-oldest surviving university. Bloop, thanks for making it this far!"        
+    },
+    {
+        id: 3,
+        img: 'Windsor Castle.png',
+        country: 'WINDSOR, UK',
+        link: 'https://www.google.com/maps/search/windsor+castle/@51.5034969,-0.678416,11z',
+        location: 'Windsor Castle',
+        startDate: '04 Nov. 2018',
+        description: 'The Windsor castle was very magical and romantic. I love seeing the historically maintained rooms and imagining what it was like to live there in the past. Funfact: Windsor Castle is the oldest and largest inhabited castle in the world!'        
+    },
+    {
+        id: 2,
+        img: 'Stonehenge.png',
+        country: 'SALISBURY, UK',
+        link: 'https://www.google.com/maps/@43.8395009,-79.3916043,15z',
+        location: 'Stonehenge',
+        startDate: '10 Nov. 2018',
+        description: 'Went on a shuttle bus to Stonehenge, it was crazy to see the huge stones in person. Very mysterious and interesting to walk around the area, there were some structures and other historical information throughout the area. How did the stones get there?! There were a lot of theries. Wish we got to see it during sunset, maybe next time! Fun fact: 2.13m of Stone 56, the tallest standing stone on the site, is buried underground - in total it measures 8.71 metres from base to tip.'        
+    }
+
+]
